eb5c139624a4cbf7b98b9669a97cccce631b1b1c
Sat-blip <76175333+Sat-blip@users.noreply.github.com>
2020-12-21 21:23:27 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'help',
 	description: 'A list of all commands and their descriptions.',
 	execute(msg, args, typicalEmbed, colour, footer, prefix) {
-		const desc = "`" + prefix + "` - prefix.\n- `addbd` - Adds your birthday.\n- `rembd` - Removes your birthday.\n- `help` - You are here.\n- `bug` - Send a bug to the developer! Be sure to include a message link!\n- `epbio` - Shows you the name, season, episode num, and desc of any episode.";
+		const desc = "`" + prefix + "` - prefix.\n- `addbd [daynum] [monthnum]` - Adds your birthday. Be sure to put a space between [daynum] and [monthnum]. Eg: `>addbd 23 04`.\n- `rembd` - Removes your birthday.\n- `help` - You are here.\n- `bug` - Send a bug to the developer! Be sure to include a message link!\n- `epbio` - Shows you the name, season, episode num, and desc of any episode.";
     msg.channel.send(typicalEmbed(desc, "Help and Info", footer, colour));
   },
 };
