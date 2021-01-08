fa7827b3c9905990ed1b32a48cfe45a24acf012c
Sat-blip <76175333+Sat-blip@users.noreply.github.com>
2021-01-08 10:40:53 +0000

    Update addtrivia.js

--- commands/addtrivia.js
+++ commands/addtrivia.js
@@ -1,16 +1,25 @@
 module.exports = {
 	name: 'addtrivia',
 	description: 'Staff only! Adds trivia points to a certain user.',
-	execute(msg, args, typicalEmbed, colour, footer, trivia) {
+	execute(msg, args, typicalEmbed, colour, footer, fs) {
     //arg[1] - amount to give.
     try {
       const PersonToGive = msg.mentions.users.first().id;
-      if(trivia.get(PersonToGive)) {
-        const CurrentPoints = parseInt(trivia.get(PersonToGive));
-        trivia.set(PersonToGive, CurrentPoints + parseInt(args[1]));
-        msg.channel.send("Done!");
-      } else {
-        trivia.set(PersonToGive, parseInt(args[1]));
+      const data = JSON.parse(fs.readFileSync("JSONs/trivia.json", "utf8"));
+      for(let i = 0; i < data.length; i++) {
+        if(data[i]["UserID"] == PersonToGive) {
+          const CurrentPoints = parseInt(data[i]["TriviaPoints"]);
+          data[i]["TriviaPoints"] = CurrentPoints + parseInt(args[1]);
+          let jsonDATA = JSON.stringify(data);
+          fs.writeFile("JSONs/trivia.json", jsonDATA, function(err) { if(err) { console.log(err); } });   
+          msg.channel.send("Done!");    
+          break;   
+        } else if(i == data.length - 1) {
+          let MyDict = { "UserID": PersonToGive, "TriviaPoints": args[1]};
+          data.push(MyDict);
+          let jsonDATA = JSON.stringify(data);
+          fs.writeFile("JSONs/trivia.json", jsonDATA, function(err) { if(err) { console.log(err); } });
+        }
       }
     } catch(e) {
       msg.channel.send("Correct usage: `$addtrivia [mention user] [amount]`");
